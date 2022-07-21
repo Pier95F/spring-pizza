9cbb86c527ddfd0114164b1d6757b5b97b9a761e
Pier95F <forcinapierpaolo@gmail.com>
2022-07-21 15:39:52 +0200

    esercizio base terminato

--- src/main/java/jana60/controller/PizzaController.java
+++ src/main/java/jana60/controller/PizzaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jana60.repository.PizzaRepository;
@@ -20,4 +22,16 @@ public class PizzaController {
 		model.addAttribute("pizza", repo.findAll());
 		return "Index";
 	}
+	
+	 @GetMapping("/add")
+	  public String AddPizza(Model model) {
+	   model.addAttribute("pizza", new jana60.model.Pizza());
+	   return "FormPizza";
+	    }
+	 
+	    @PostMapping("/add")
+	    public String SavePizza(@ModelAttribute("pizza") jana60.model.Pizza addPizza) {
+	            repo.save(addPizza);
+	            return "redirect:/";	
+	    }
 }
